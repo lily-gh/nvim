dd1724425a8b16a561436f56df1f1f3713192665
Lily <8723107+lily-gh@users.noreply.github.com>
2025-12-29 14:46:28 +0100

    feat: add local palette-picker plugin during development

---
+++ lua/plugins/palette-picker.lua
@@ -0,0 +1,9 @@
+return {
+  -- local plugin
+  dir = "/Users/lily/projects/palette-picker.nvim",
+  name = "palette-picker",
+
+  -- load immediately so :PalettePicker exists at startup
+  lazy = false,
+}
+
